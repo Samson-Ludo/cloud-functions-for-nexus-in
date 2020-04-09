23aeadd788ab0a9ff107fb780c4aef26070f091e
Samson Okaludo <43629180+Samson-Ludo@users.noreply.github.com>
2020-04-09 08:40:47 +0100

    Update config.js

--- functions/util/config.js
+++ functions/util/config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  apiKey: "AIzaSyAMgI3glYbpv3Wcq7UhwMl3kedvu2NpEFc",
-  authDomain: "nexus-in.firebaseapp.com",
-  databaseURL: "https://nexus-in.firebaseio.com",
-  projectId: "nexus-in",
-  storageBucket: "nexus-in.appspot.com",
-  messagingSenderId: "735753437019",
-  appId: "1:735753437019:web:6844fdd8558046731f247e",
-  measurementId: "G-H0J4ZPN438"
+  apiKey: [API_KEY],
+  authDomain: [AUTH_DOMAIN],
+  databaseURL: [DATABASE_URL],
+  projectId: [PROJECT_ID],
+  storageBucket: [STORAGE_BUCKET],
+  messagingSenderId: [MESSAGING_SENDER_ID],
+  appId: [APP_ID],
+  measurementId: [MEASUREMENT_ID]
 };
